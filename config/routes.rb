8b3f83d7cbb2a1eb57485a477aa0cc8d8b4a1768
EC2 Default User <ec2-user@ip-172-31-21-218.ap-southeast-1.compute.internal>
2019-03-27 03:09:50 +0000

    third

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   root 'home#top'
   resources :posts , :only => [:show, :index, :new, :create, :edit, :update, :destroy]
   resources :users, :only => [:show, :index, :edit, :update]
-
+# afafdfa
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
